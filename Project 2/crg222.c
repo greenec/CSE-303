5dbdfa786cfeb36683f037ca02c2eaadb992e7bf
Connor Greene <connor@connorgreene.net>
2020-02-06 23:02:04 -0500

    Added added sleep command and background processes

--- Project 2/crg222.c
+++ Project 2/crg222.c
@@ -20,14 +20,36 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-enum {
-	MAXLINE = 200
-};
+const int MAXLINE = 200;
+
+/* 
+ * Reverses a given string without modifying the original.
+ */
+void strrev(const char* const str, char* out)
+{
+	char *p1, *p2;
+
+	if (! str || ! *str)
+	{
+		return;
+	}
+	
+	// copy the input to the output string so it can be reversed
+	strcpy(out, str);
+	
+	// use two pointers and XOR to reverse the output string
+	for (p1 = out, p2 = out + strlen(out) - 1; p2 > p1; ++p1, --p2)
+	{
+		*p1 ^= *p2;
+		*p2 ^= *p1;
+		*p1 ^= *p2;
+	}
+}
 
 /*
  * Prints the ASCII clear command to reset the terminal.
  */
-void clearScreen()
+void clearCmd()
 {
 	printf("\e[1;1H\e[2J");
 }
@@ -35,23 +57,23 @@ void clearScreen()
 /*
  * Prints the contents of a given directory.
  */
-void listDirectory(const char* const s)
+void directoryCmd(const char* const s)
 {
 	// pointer for directory 
 	struct dirent *de;
 
-	char s2[MAXLINE];
-	strcpy(s2, s);
+	char cmd[MAXLINE];
+	strcpy(cmd, s);
 	
 	// use strtok to get rid of line feed then find the first token
-	strtok(s2, "\n");
-	strtok(&s2[2], " ");
+	strtok(cmd, "\n");
+	strtok(&cmd[2], " ");
 	char* dir = strtok(NULL, " ");
 	
 	// if no directory is specified, read the current directory
 	if (dir == NULL)
 	{
-		dir = s2;
+		dir = cmd;
 		strcpy(dir, ".");
 	}
 
@@ -75,7 +97,10 @@ void listDirectory(const char* const s)
 	closedir(dr); 
 }
 
-void runCommand(const char* const s)
+/* 
+ * Runs the given command with its arguments.
+ */
+void runCmd(const char* const s)
 {
 	// make a copy of the input string
 	char cmd[MAXLINE];
@@ -113,10 +138,69 @@ void runCommand(const char* const s)
 }
 
 /*
+ * Puts the process to sleep for the given duration.
+ */
+void sleepCmd(const char* const s)
+{
+	// make a copy of the input string
+	char cmd[MAXLINE];
+	strcpy(cmd, s);
+	
+	// user strtok to get rid of line feed then find the first token
+	strtok(cmd, "\n");
+	strtok(&cmd[4], " ");
+	char* ptr = strtok(NULL, " ");
+	
+	int duration = atoi(ptr);
+	
+	sleep(duration);
+}
+
+/*
+ * Determines if the input string is blank.
+ */
+int isBlankInput(const char* const s)
+{
+	char str[strlen(s)];
+	strcpy(str, s);
+	
+	// remove leading spaces and newline
+	char* ptr = strtok(str, " \n");
+	
+	// return true if it's a null pointer
+	return ptr == NULL;
+}
+
+/*
+ * Determines whether the given command ends with an ampersand (&)
+ */
+int isBackgroundCmd(const char* const s)
+{
+	// make a reversed copy of the input command
+	char reversed[strlen(s)];
+	strrev(s, reversed);
+	
+	// remove any trailing spaces and newline
+	char* ptr = strtok(reversed, " \n");
+	
+	if (ptr != NULL)
+	{
+		return ptr[0] == '&';
+	}
+	
+	return 0;
+}
+
+/*
  * This function processes the command.
  */
 void processLine(const char* const s)
 {
+	if (isBlankInput(s))
+	{
+		return;
+	}
+	
     // output the command entered by the user
 	printf("Command received = %s\n", s);
 
@@ -142,7 +226,7 @@ void processLine(const char* const s)
 		const char* const clearStr = "clr";
 		if (strncmp(s, clearStr, strlen(clearStr)) == 0)
 		{
-			clearScreen();
+			clearCmd();
 			exit(0);
 		}
 		
@@ -154,7 +238,7 @@ void processLine(const char* const s)
 		const char* const directoryStr = "dir";
 		if (strncmp(s, directoryStr, strlen(directoryStr)) == 0)
 		{
-			listDirectory(s);
+			directoryCmd(s);
 			exit(0);
 		}
 		
@@ -164,9 +248,21 @@ void processLine(const char* const s)
 		 * Description: Runs the specified command with its arguments.
 		 */
 		const char* const runStr = "run";
-		if(strncmp(s, runStr, strlen(runStr)) == 0)
+		if (strncmp(s, runStr, strlen(runStr)) == 0)
 		{
-			runCommand(s);
+			runCmd(s);
+			exit(0);
+		}
+		
+		/*
+		 * Command: Sleep
+		 * Usage "sleep <duration>"
+		 * Description: Sleeps for <duration> seconds.
+		 */
+		const char* const sleepStr = "sleep";
+		if (strncmp(s, sleepStr, strlen(sleepStr)) == 0)
+		{
+			sleepCmd(s);
 			exit(0);
 		}
 	
@@ -176,6 +272,11 @@ void processLine(const char* const s)
 	}
 	else
 	{
+		if (isBackgroundCmd(s))
+		{
+			return;
+		}
+
 		// parent waits for the child to finish
 		wait(NULL);
 	}
